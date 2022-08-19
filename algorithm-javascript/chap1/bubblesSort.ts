2a1de7513fbb5e4212b6ab36052ac1c68e6c8e64
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-08-19 22:28:23 +0900

    fix var

--- algorithm-javascript/chap1/bubblesSort.ts
+++ algorithm-javascript/chap1/bubblesSort.ts
@@ -1,9 +1,8 @@
 const bubblesSort = (nums: number[]): number[] => {
-	let temp;
 	for (let i = 0; i < nums.length - 1; i++) {
 		for (let j = 0; j < nums.length - 1; j++) {
 			if (nums[j] > nums[j + 1]) {
-				temp = nums[j]
+				const temp = nums[j]
 				nums[j] = nums[j + 1]
 				nums[j + 1] = temp
 			}
